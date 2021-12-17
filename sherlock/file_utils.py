49a77de0e212c07ed912b231f31f0e257c6823e4
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2021-12-17 13:06:33 +0100

    Use 'pyproject.toml' configuration file.
    Configuration file can be loaded from --config. If not provided Sherlock will try to find 'pyproject.toml' file in source path
    directories.
    Add unit tests for file utils.

--- sherlock/file_utils.py
+++ sherlock/file_utils.py
@@ -16,3 +16,40 @@ def get_gitignore(root: Path) -> PathSpec:
         with gitignore.open(encoding="utf-8") as gf:
             lines = gf.readlines()
     return PathSpec.from_lines("gitwildmatch", lines)
+
+
+def find_project_root(paths) -> Path:
+    """Return a directory containing .git or pyproject.toml.
+    That directory will be a common parent of all files and directories
+    passed in `paths`.
+    If no directory in the tree contains a marker that would specify it's the
+    project root, the root of the file system is returned.
+    """
+    if paths is None:
+        return Path.cwd()
+
+    path_srcs = [Path(Path.cwd(), path).resolve() for path in paths]
+
+    # A list of lists of parents for each 'path'. 'path' is included as a
+    # "parent" of itself if it is a directory
+    src_parents = [list(path.parents) + ([path] if path.is_dir() else []) for path in path_srcs]
+
+    common_base = max(
+        set.intersection(*(set(parents) for parents in src_parents)),
+        key=lambda path: path.parts,
+    )
+
+    for directory in (common_base, *common_base.parents):
+        if (
+                (directory / ".git").exists()
+                or (directory / "pyproject.toml").is_file()
+        ):
+            return directory
+    return directory
+
+
+def find_file_in_project_root(config_name, root):
+    for parent in (root, *root.parents):
+        if (parent / ".git").exists() or (parent / config_name).is_file():
+            return parent / config_name
+    return parent / config_name
---
+++ tests/utest/test_file_utils.py
@@ -0,0 +1,70 @@
+import os
+from pathlib import Path
+import contextlib
+
+import pytest
+
+from sherlock.file_utils import find_project_root, get_gitignore, find_file_in_project_root
+
+
+@pytest.fixture
+def path_to_test_data():
+    return Path(Path(__file__).parent.parent, "test_data")
+
+
+@contextlib.contextmanager
+def working_directory(path):
+    """Changes working directory and returns to previous on exit"""
+    prev_cwd = Path.cwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(prev_cwd)
+
+
+class TestFileUtils:
+    def test_find_project_root(self, path_to_test_data):
+        source = path_to_test_data / "configs" / "pyproject"
+        root = find_project_root((source,))
+        assert root == source
+
+    def test_find_project_root_without_sources(self, path_to_test_data):
+        source = path_to_test_data / "configs" / "pyproject"
+        with working_directory(source):
+            root = find_project_root(None)
+            assert root == source
+
+    def test_find_project_root_multiple_sources(self, path_to_test_data):
+        common = path_to_test_data / "configs" / "nested_pyproject"
+        sources = (common / "1" / "2" / "test.robot", common / "2")
+        root = find_project_root(sources)
+        assert root == common
+
+    def test_find_project_root_no_pyproject(self):
+        source = Path.home()
+        root = find_project_root((source,))
+        assert root == Path(source.parts[0])
+
+    def test_find_file_in_project_root(self, path_to_test_data):
+        source = path_to_test_data / "configs" / "nested_pyproject" / "2"
+        file = find_file_in_project_root("pyproject.toml", source)
+        assert file == source / "pyproject.toml"
+
+    def test_find_file_in_project_root_no_file(self, path_to_test_data):
+        source = path_to_test_data / "configs" / "no_pyproject"
+        file = find_file_in_project_root("pyproject.toml", source)
+        # if file is not found it will find it in parents - in this case top of the repository
+        assert file == path_to_test_data.parent.parent / "pyproject.toml"
+
+    def test_find_file_in_project_root_outside_repo(self, path_to_test_data):
+        source = Path.home()
+        file = find_file_in_project_root("pyproject.toml", source)
+        # if file is not found anywhere, it will return Path({root}:/{file})
+        assert file == Path(source.parts[0]) / "pyproject.toml"
+
+    def test_get_gitignore(self, path_to_test_data):
+        gitignore = path_to_test_data / "gitignore"
+        spec = get_gitignore(gitignore)
+        assert spec.match_file("file.resource")
+        assert not spec.match_file("file.robot")
