00073eb0a793d36363e0d9a94404a0a6d37f23d6
Bartlomiej Hirsz <bartek.hirsz@gmail.com>
2022-01-06 16:10:38 +0100

    Generate HTML with collabsible/foldable nodes

--- sherlock/report/print.py
+++ sherlock/report/print.py
@@ -1,11 +1,13 @@
 import textwrap
 
+from sherlock.model import DIRECTORY_TYPE
+
 
 def log_directory(directory, log_handle, indent=""):
     print(textwrap.indent(str(directory), indent), file=log_handle)
     indent += "    "
     for resource in directory.children:
-        if resource.type == "Tree":
+        if resource.type == DIRECTORY_TYPE:
             log_directory(resource, log_handle, indent)
         else:
             print(textwrap.indent(str(resource), indent), file=log_handle)
