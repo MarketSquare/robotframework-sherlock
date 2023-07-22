390dd94b9444fd081b0240ad8f5ed794cb7c2c14
Bartłomiej Hirsz <bartek.hirsz@gmail.com>
2023-07-22 18:56:51 +0200

    Allow to skip running robot in atests (#79)

--- tests/atest/test_example_workflow.py
+++ tests/atest/test_example_workflow.py
@@ -12,6 +12,7 @@ class TestExampleWorkflow(AcceptanceTest):
     def test_with_xml_output(self):
         robot_output = self.ROOT / "output.xml"
         source = self.ROOT / self.TEST_PATH
+        self.run_robot()
         run_sherlock(robot_output=robot_output, source=source)
 
     def test_without_xml_output(self):
@@ -22,6 +23,7 @@ class TestExampleWorkflow(AcceptanceTest):
     def test_with_report(self, report_type):
         robot_output = self.ROOT / "output.xml"
         source = self.ROOT / self.TEST_PATH
+        self.run_robot()
         runner = run_sherlock(robot_output=robot_output, source=source, report=[report_type])
         output = runner.config.root / f"sherlock_tests.{report_type}"
         assert output.is_file()
