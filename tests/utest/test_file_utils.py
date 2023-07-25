5e04bedb435f164df9a1644fe510082a5dd3a906
Bartłomiej Hirsz <bartek.hirsz@gmail.com>
2023-07-25 15:45:56 +0200

    Replace fixture with const variable in tests (#86)

--- tests/utest/test_file_utils.py
+++ tests/utest/test_file_utils.py
@@ -6,10 +6,7 @@ import pytest
 
 from sherlock.file_utils import find_file_in_project_root, find_project_root, get_gitignore
 
-
-@pytest.fixture
-def path_to_test_data():
-    return Path(__file__).parent.parent / "test_data"
+TEST_DATA_DIR = Path(__file__).parent.parent / "test_data"
 
 
 @contextlib.contextmanager
@@ -24,19 +21,19 @@ def working_directory(path):
 
 
 class TestFileUtils:
-    def test_find_project_root(self, path_to_test_data):
-        source = path_to_test_data / "configs" / "pyproject"
+    def test_find_project_root(self):
+        source = TEST_DATA_DIR / "configs" / "pyproject"
         root = find_project_root((source,))
         assert root == source
 
-    def test_find_project_root_without_sources(self, path_to_test_data):
-        source = path_to_test_data / "configs" / "pyproject"
+    def test_find_project_root_without_sources(self):
+        source = TEST_DATA_DIR / "configs" / "pyproject"
         with working_directory(source):
             root = find_project_root(None)
             assert root == source
 
-    def test_find_project_root_multiple_sources(self, path_to_test_data):
-        common = path_to_test_data / "configs" / "nested_pyproject"
+    def test_find_project_root_multiple_sources(self):
+        common = TEST_DATA_DIR / "configs" / "nested_pyproject"
         sources = (common / "1" / "2" / "test.robot", common / "2")
         root = find_project_root(sources)
         assert root == common
@@ -46,25 +43,25 @@ class TestFileUtils:
         root = find_project_root((source,))
         assert root == Path(source.parts[0])
 
-    def test_find_file_in_project_root(self, path_to_test_data):
-        source = path_to_test_data / "configs" / "nested_pyproject" / "2"
+    def test_find_file_in_project_root(self):
+        source = TEST_DATA_DIR / "configs" / "nested_pyproject" / "2"
         file = find_file_in_project_root("pyproject.toml", source)
         assert file == source / "pyproject.toml"
 
-    def test_find_file_in_project_root_no_file(self, path_to_test_data):
-        source = path_to_test_data / "configs" / "no_pyproject"
+    def test_find_file_in_project_root_no_file(self):
+        source = TEST_DATA_DIR / "configs" / "no_pyproject"
         file = find_file_in_project_root("pyproject.toml", source)
         # if file is not found it will find it in parents - in this case top of the repository
-        assert file == path_to_test_data.parent.parent / "pyproject.toml"
+        assert file == TEST_DATA_DIR.parent.parent / "pyproject.toml"
 
-    def test_find_file_in_project_root_outside_repo(self, path_to_test_data):
+    def test_find_file_in_project_root_outside_repo(self):
         source = Path.home()
         file = find_file_in_project_root("pyproject.toml", source)
         # if file is not found anywhere, it will return Path({root}:/{file})
         assert file == Path(source.parts[0]) / "pyproject.toml"
 
-    def test_get_gitignore(self, path_to_test_data):
-        gitignore = path_to_test_data / "gitignore"
+    def test_get_gitignore(self):
+        gitignore = TEST_DATA_DIR / "gitignore"
         spec = get_gitignore(gitignore)
         assert spec.match_file("file.resource")
         assert not spec.match_file("file.robot")
